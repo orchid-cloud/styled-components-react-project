b913d2adfa0ab71af9f63636a7b5381129253473
NZ <n.prokopyk@gmail.com>
2024-08-01 09:19:37 +0200

    component card created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
 import { Container } from './components/styles/Container.styled';
 import GlobalStyles from './components/styles/Global';
+import content from './content';
 
 const theme = {
   colors: {
@@ -19,7 +20,9 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => (
+            <p>{item.title}</p>
+          ))}
         </Container>
       </>
     </ThemeProvider>
