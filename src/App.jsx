97a192290859a244dabfa4f0475de679b5e45a64
NZ <n.prokopyk@gmail.com>
2024-08-01 09:26:51 +0200

    component card created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
+import Card from './components/Card';
 import { Container } from './components/styles/Container.styled';
 import GlobalStyles from './components/styles/Global';
 import content from './content';
@@ -21,7 +22,7 @@ function App() {
         <Header />
         <Container>
           {content.map((item, index) => (
-            <p>{item.title}</p>
+            <Card key={index} item={item} />
           ))}
         </Container>
       </>
