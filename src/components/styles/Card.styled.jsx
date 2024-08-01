c356d391ed8d3c17a4d68b391c484589ec0607bb
NZ <n.prokopyk@gmail.com>
2024-08-01 10:17:07 +0200

    footer component created

--- src/components/styles/Card.styled.jsx
+++ src/components/styles/Card.styled.jsx
@@ -8,4 +8,17 @@ export const StyledCard = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
   margin: 40px 0;
   padding: 60px;
+  flex-direction: ${({ layout }) => layout};
+
+  img {
+    width: 80%;
+  }
+
+  & > div {
+    flex: 1;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    flex-direction: column;
+  }
 `;
